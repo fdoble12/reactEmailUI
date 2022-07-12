36fd04dfbc67a95fed57f0e37d786bd1a9a6dc9b
fdoble12 <80149315+fdoble12@users.noreply.github.com>
2022-07-12 13:29:46 +0800

    Update EmailContainer.js

--- src/components/EmailContainer.js
+++ src/components/EmailContainer.js
@@ -1,14 +1,11 @@
 import CollapsibleEmail from "./CollapsibleEmail"
-import data from "../data"
 
 export const EmailContainer = () => {
-  var emails = data.data;
 
   return (
     <div className="emailContainer">
-        {emails.forEach((curr) =>{
-          <CollapsibleEmail/>
-        })}
+        <CollapsibleEmail/>
+        <CollapsibleEmail/>
     </div>
   )
 }
